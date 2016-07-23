5a93cc964ece617aa0249a24305195f5982e6c2a
ankur-rajput <rajputankur023@gmail.com>
2016-07-24 03:00:09 +0530

    basic project stucture for Spring boot and loginRegister page added

---
+++ src/main/java/com/tcs/friendlier/controllers/Application.java
@@ -0,0 +1,24 @@
+package com.tcs.friendlier.controllers;
+
+import java.util.Arrays;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		ApplicationContext applicationContext = SpringApplication.run(Application.class, args);
+
+		System.out.println("Let's inspect the beans provided by Spring Boot:");
+
+		String[] beanNames = applicationContext.getBeanDefinitionNames();
+		Arrays.sort(beanNames);
+		for (String beanName : beanNames) {
+			System.out.println(beanName);
+		}
+	}
+
+}
